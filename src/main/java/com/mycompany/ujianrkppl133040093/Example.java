db90fcae7367a9219d332d76f9fbca49b68a639c
vikicandra <vikicandrata13@gmail.com>
2016-05-13 16:32:57 +0700

    second commit

--- src/main/java/com/mycompany/ujianrkppl133040093/Example.java
+++ src/main/java/com/mycompany/ujianrkppl133040093/Example.java
@@ -5,10 +5,25 @@
  */
 package com.mycompany.ujianrkppl133040093;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author VIKI
  */
+
+
 public class Example {
+    private static Logger log = Logger.getLogger(Example.class);
+    
+    public static void main(String[] args) {
+        log.debug("Ini adalah debug message : contoh log4j");
+        log.info("Ini adalah info message : contoh log4j");
+        log.fatal("Ini adalah fatal message : contoh log4j");
+        log.warn("Ini adalah warn message : contoh log4j");
+        log.error("Ini adalah error message : contoh log4j");
+    }
+    
+    
     
 }
